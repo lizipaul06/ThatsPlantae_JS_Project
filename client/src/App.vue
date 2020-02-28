5713ea6bbab4a09bb11f626790e15e5ffd722e52
PorcelainAddict <user@Users-MacBook-Pro.local>
2020-02-28 16:42:27 +0000

    setup_front_end_files

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="body">
+    <p>That's Plantae!</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+.body {
+  font-family: cursive;
+  font-size: 60px;
+}
+</style>
---
+++ client/src/services/PlantService.js
@@ -0,0 +1,37 @@
+const baseURL = 'http://localhost:9000/api/plants/' //BaseURL may need to change due to back-end.
+
+ export default {
+
+   //this function shall get us our plants(nae details)
+   getPlants(){
+     return fetch(baseURL)
+     .then(res => res.json())
+   },
+
+   //we should be able to post something to our DB.
+   postPlant(payload){
+     return fetch(baseURL, {
+       method: 'POST',
+       body: JSON.stringify(payload),
+       headers: {'Content-Type': 'application/json'}
+     })
+     .then(res => res.json())
+   },
+
+   //we can update our garden.
+   updateGarden(id, payload){
+     return fetch(baseURL + id, {
+       method: 'PUT',
+       body: JSON.stringify(payload),
+       headers: {'Content-Type': 'application/json'}
+     })
+     .then(res => res.json())
+   },
+
+   //one of our plants from out db can be deleted
+   deletePlant(id, payload){
+     return fetch(baseURL + id, {
+       method: 'DELETE'
+     })
+   }
+ }
