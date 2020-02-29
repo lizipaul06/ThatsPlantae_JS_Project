588c9042b8ae5980eea98b2812174b84f5d53ec3
Elizabeth Paul <elizabethpaul@Elizabeths-MacBook-Pro.local>
2020-02-29 12:27:13 +0000

    more attempted

--- client/src/server/index.js
+++ client/src/server/index.js
@@ -14,17 +14,34 @@ app.get("/hello", (req, res) => {
   .then(data => res.send(data))
 });
 
+// app.get("/plant", (req, res) => {
+//   const url = req.body.json()
+//   fetch(url)
+//   .then(res => res.json())
+//   .then(data => res.send(data))
+//
+// });
+
+// app.get("/plant/:id", (req,res) =>{
+//   fetch(payload =("https://trefle.io/api/plants/".concat(token)),{
+//   method: 'POST',
+//   body: JSON.stringify(payload),
+//   headers: {'Content-Type': 'application/json'}
+// })
+//   .then(res => res.json())
+// });
+
 
 // app.get("/hello", (req, res) => {
 //   fetch("https://trefle.io/api/plants/:id").concat(token) )
 // //   .then(res => res.json())
 // //   .then(data => res.send(data))
 // // });
-
-getInfo(link){
-  fetch(`${link}`.concat(token))
-  .then(res => res.json())
-};
+//
+// getInfo(link){
+//   fetch(`${link}`.concat(token))
+//   .then(res => res.json())
+// };
 
 app.listen(9000, () => {
   console.log("Server is listening on port: 9000");
