efdf338f8e30c99feb3c23dc8def61e815a90ab5
Meredith Gallop <meredith@trtl.co.uk>
2020-03-01 12:11:58 +0000

    hosted background locally

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,8 +2,7 @@
   <div class="body">
     <p id="title">That's Plantae!</p>
     <search :plants="plants" />
-    <!-- <plant-list :plants="plants"></plant-list> -->
-    <p>That's Plantae!</p>
+    <plant-list :plants="plants"></plant-list>
     <ul>
     <li v-for="(plant, index) in this.plantData" :key="index" :plant="plant"> {{plant.common_name}} </li>
     </ul>
@@ -39,23 +38,16 @@ export default {
       let fetches = []
       this.plantData.map(id => ( fetches.push(PlantService.getPlant(id))))
       console.log(fetches)
- // }).catch((fetches) => {console.log(fetches)}).then(res => res.json())
- //  .then(res => {
- //      console.log(res.data); // Here I get what I need
- //      this.plantDetails = res.data;
-// .then(data => console.log(data))
-        })
+    })
+}
+}
+</script>
 
-      // Promise.resolve(this.plantData)
-      // .then(data => this.plantData = data.filter(data => data.PromiseStatus = "resolved"))
-
-
-<<<<<<< HEAD
 <style lang="css" scoped>
 .body {
   font-family: cursive;
   font-size: 60px;
-  background: url(https://images.unsplash.com/photo-1425736317462-a103b1303a35?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80) no-repeat center center fixed;
+  background: url('../public/background.jpeg') no-repeat center center fixed;
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
@@ -69,30 +61,4 @@ export default {
   color: white;
 }
 
-
-
-
 </style>
-=======
-//
-// })
-}
-}
-
-// mounted(){
-//   PlantService.getPlants()
-//   .then(plants => this.plants = plants)
-//   .then(() => {
-//       let fetches = []
-//       fetches.push(PlantService.getPlant(this.plants[0].id))
-//       fetches.push(PlantService.getPlant(this.plants[1].id))
-//       Promise.all(fetches)
-//       .then(data => console.log(data))
-//   })
-// }
-    </script>
-
-    <style lang="css" scoped>
-
-    </style>
->>>>>>> develop
