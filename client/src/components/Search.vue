2c1c7af7e2b0f659c241c3293652f07e9d07a936
PorcelainAddict <user@Users-MacBook-Pro.local>
2020-03-02 14:26:11 +0000

    started working on fuse search

--- client/src/components/Search.vue
+++ client/src/components/Search.vue
@@ -1,12 +1,21 @@
 <template lang="html">
-  <form v-on: submit.prevent>
-    <input id="plantBar" type="text" v-model="search"
-    placeholder="search through our 'plantae'-ful array of plants..." v-on:keyup="searchForPlant">
-    <!-- <select v-on:change="handleSelect" v-model="selectedPlant">
-      <option disabled value="">Select a Plant....</option>
-      <option v-for="plant in plants" :value="plant">{{plant.common_name}}</option>
-    </select> -->
-  </form>
+  <div class="">
+    <VueFuseParams
+    placeholder="search through our 'plantae'-ful array of plants..."
+    event-name='results'
+    :list=''
+    :keys="['common_name', 'family_common_name']"
+    />
+
+  </div>
+  <!-- <form v-on: submit.prevent>
+  <!-- <input id="plantBar" v-model="search"
+  placeholder="search through our 'plantae'-ful array of plants..." v-on:keyup="searchForPlant"> -->
+  <!-- <select v-on:change="handleSelect" v-model="selectedPlant">
+  <option disabled value="">Select a Plant....</option>
+  <option v-for="plant in plants" :value="plant">{{plant.common_name}}</option>
+</select> -->
+<!-- </form> -->
 
 </template>
 
@@ -15,34 +24,51 @@
 
 <script>
 import {eventBus} from '../main.js';
+import VueFuseParams from '../components/VueFuseParams.vue';
 
 export default {
   name: "plant-filter-form",
+  props: ["plants"],
+  components: {
+    VueFuseParams
+  },
   data(){
     return {
-      "search": "",
+      results: [],
       "selectedPlant": {},
     }
   },
-  props: ["plants"],
   methods: {
-    searchForPlant(){
-      let foundPlant = this.plants.find((plant) => {
-        return
-        plant.common_name.toLowerCase().indexOf(this.search.toLowerCase()) > -1
-      })
-      this.selectedPlant = foundPlant
-
-      eventBus.$emit('plant-selected',
-      this.selectedPlant)
-    },
-    handleSelect(){
-      this.search = "",
-      eventBus.$emit('plant-selected',
-      this.selectedPlant)
+    runSearch() {
+      this.$search('Plant', this.plants, { keys:['common_name']}
+      .then(result => {
+        this.results = result
+      }))
     }
+  },
+  created () {
+    this.$on('results', results => {
+      this.results = results
+    })
   }
 }
+//   methods: {
+//     searchForPlant(){
+//       let foundPlant = this.plants.find((plant) => {
+//         return
+//         plant.common_name.toLowerCase().indexOf(this.search.toLowerCase()) > -1
+//       })
+//       this.selectedPlant = foundPlant
+//
+//       eventBus.$emit('plant-selected',
+//       this.selectedPlant)
+//     },
+//     handleSelect(){
+//       this.search = "",
+//       eventBus.$emit('plant-selected',
+//       this.selectedPlant)
+//     }
+//   }
 </script>
 
 <style lang="css" scoped>
